f348dd3b170301e9b2f734aa0e3eb43166a3d157
Horodrigo <hodrigocastelo@gmail.com>
2019-12-04 09:13:11 -0300

    MRU(LRU) adicionado

--- src/br/edu/ifce/threads/Nur.java
+++ src/br/edu/ifce/threads/Nur.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
 package br.edu.ifce.threads;
 
@@ -41,6 +39,7 @@ public class Nur extends Thread {
         
 
         this.acertos = -1;
+        System.out.println("Frames: " + qFrames + ". NUR: " + this.acertos);
         this.tela.setResultado(TipoAlgoritmo.NUR, this.acertos);
     }
 
